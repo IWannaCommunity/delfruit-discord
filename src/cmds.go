16096c95da0ef2740bd64d81e2468acfe0b4a427
Starz0r <starz0r@starz0r.com>
2019-07-18 03:08:23 -0500

    abort if the command isn't actually called
    For some reason the difference on this file between the last change is huge. I imagine it's because gofmt sucks, but there isn't much I can do about that.

--- src/cmds.go
+++ src/cmds.go
@@ -9,57 +9,59 @@ import (
 
 func publish(s *discordgo.Session, j *discordgo.MessageCreate) {
 	if !j.Author.Bot { // check if not a bot
-		member, err := discord.State.Member(j.GuildID, j.Author.ID)
-		published := *new([]*discordgo.MessageEmbed)
-		if err != nil {
-			fmt.Println("member was not found or did not exist")
-			return
-		}
-		for i := range member.Roles { // check if privileged
-			if member.Roles[i] == privilegedRole {
-				args := strings.Split(j.Content, " ")
-
-				if len(args) > 1 {
-					for i := 1; i < len(args); i++ {
-						embed := single(args[i], "id")
-						if embed == nil {
-							s.ChannelMessageSend(j.ChannelID, strings.Join([]string{"No Game with ID ", args[i]}, ""))
-							return
-						} else {
-							published = append(published, embed)
-						}
-
-						switch args[0] {
-						case "!release":
-							embed = amend(embed, "release")
-						case "!update":
-							embed = amend(embed, "update")
-						case "!featured":
-							embed = amend(embed, "featured")
-						}
-
-						s.ChannelMessageSendEmbed(subscribedChannel, embed)
-					}
-					break
-				} else {
-					s.ChannelMessageSend(j.ChannelID, "Parameters were not supplied.")
-					return
-				}
-			} else {
-				args := strings.Split(j.Content, " ")
-
-				fmt.Println(strings.Join([]string{j.Author.Username, " did not meet role requirement for command [", args[0], "]"}, ""))
-
-				fmt.Println()
+		if strings.Contains(j.Content, "!release") || strings.Contains(j.Content, "!update") {
+			member, err := discord.State.Member(j.GuildID, j.Author.ID)
+			published := *new([]*discordgo.MessageEmbed)
+			if err != nil {
+				fmt.Println("member was not found or did not exist")
 				return
 			}
-		}
+			for i := range member.Roles { // check if privileged
+				if member.Roles[i] == privilegedRole {
+					args := strings.Split(j.Content, " ")
 
-		embeds := []string{"Published to subscribed channels!", "```"}
-		for i := range published {
-			embeds = append(embeds, published[i].Title, "\n")
+					if len(args) > 1 {
+						for i := 1; i < len(args); i++ {
+							embed := single(args[i], "id")
+							if embed == nil {
+								s.ChannelMessageSend(j.ChannelID, strings.Join([]string{"No Game with ID ", args[i]}, ""))
+								return
+							} else {
+								published = append(published, embed)
+							}
+
+							switch args[0] {
+							case "!release":
+								embed = amend(embed, "release")
+							case "!update":
+								embed = amend(embed, "update")
+							case "!featured":
+								embed = amend(embed, "featured")
+							}
+
+							s.ChannelMessageSendEmbed(subscribedChannel, embed)
+						}
+						break
+					} else {
+						s.ChannelMessageSend(j.ChannelID, "Parameters were not supplied.")
+						return
+					}
+				} else {
+					args := strings.Split(j.Content, " ")
+
+					fmt.Println(strings.Join([]string{j.Author.Username, " did not meet role requirement for command [", args[0], "]"}, ""))
+
+					fmt.Println()
+					return
+				}
+			}
+
+			embeds := []string{"Published to subscribed channels!", "```"}
+			for i := range published {
+				embeds = append(embeds, published[i].Title, "\n")
+			}
+			embeds = append(embeds, "```")
+			s.ChannelMessageSend(j.ChannelID, strings.Join(embeds, ""))
 		}
-		embeds = append(embeds, "```")
-		s.ChannelMessageSend(j.ChannelID, strings.Join(embeds, ""))
 	}
 }
